1c6220628a5c046dca34a1aa698d6ca9eb95165d
mwilsonbrown <wils1582@msu.edu>
2024-07-03 14:32:38 -0400

    update mapping file generation, initialize pruning

---
+++ ahmm_LD_prune.sh
@@ -0,0 +1,22 @@
+
+
+
+
+# Ancestry HMM LD Pruning
+# Prune LD SNPs in parental populations for Ancestry HMM analysis
+
+# M. Wilson Brown
+# July 3, 2024
+
+# purge modules
+module purge
+# Load modules
+ml 
+
+# Sites in LD in C. rubella population
+
+# Sites in LD in E. Asia C. bursa-pastoris population
+
+# Combine sites
+
+# Prune from admixed individuals
