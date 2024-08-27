223dd33310993cb929c63bee7cd7dfaaf61d9ce2
mwilsonbrown <wils1582@msu.edu>
2024-08-27 13:16:12 -0400

    add PIXY script and population file

---
+++ PIXY_cbp.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+#SBATCH --job-name=PIXYcbp
+#SBATCH --nodes=10
+#SBATCH --cpus-per-task=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=0-05:00:00
+#SBATCH --partition=josephsnodes
+#SBATCH --account=josephsnodes
+#SBATCH --mem-per-cpu=8G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=wils1582@msu.edu
+#SBATCH --output=/mnt/scratch/wils1582/slurm/slurm-%A.out
+# Site level population statistics with PIXY
+# August 27, 2024
+
+# PIXY population file generation in capsella_introgression_popgen.Rmd
+
+#### VARS
+VCF=/mnt/home/wils1582/allSites_CBP_final.filtered.vcf.gz
+POPS=/mnt/home/wils1582/capsella_introgression/pixy_pops.txt
+OUTDIR=/mnt/home/wils1582
+PREFIX=allSites_CBP
+
+# Optional VARS
+#BED=
+
+# # first, make sure the vcf is indexed
+# tabix $VCF 
+# 
+# # Run PIXY on each chromosome in windows
+# pixy --stats pi fst dxy \
+# --vcf $VCF \
+# --populations $POPS \
+# --window_size 10000 \
+# --n_cores 10 \
+# --output_folder "$OUTDIR"/ \
+# --output_prefix "$PREFIX"
+
+# Run PIXY on each site (much slower than windows)
+pixy --stats pi fst dxy \
+--vcf $VCF \
+--populations $POPS \
+--window_size 1 \
+--n_cores 10 \
+--output_folder "$OUTDIR"/ \
+--output_prefix "$PREFIX"
+
+# # Run PIXY on intervals using a bed file
+# pixy --stats pi fst dxy \
+# --vcf $VCF \
+# --populations $POPS \
+# --bed_file genomic_windows.bed
