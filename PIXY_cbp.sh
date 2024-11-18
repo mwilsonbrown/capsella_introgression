c18f841ed38fc0ce0cd66b7232417f0515c4e2d6
mwilsonbrown <wils1582@msu.edu>
2024-11-18 12:32:39 -0500

    make VCF SNPs only; changed input bed file to be 1-based; use the bedfile instead of a sites file

--- PIXY_cbp.sh
+++ PIXY_cbp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH --job-name=4foldPI
+#SBATCH --job-name=Pi4_bed_1based
 #SBATCH --nodes=10
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
@@ -25,21 +25,38 @@ cd /mnt/scratch/wils1582
 source /mnt/home/wils1582/miniconda3/bin/activate pixy
 
 #### VARS
-VCF=/mnt/research/josephslab/Maya/capsella/vcf/filtered/CBP_allsites_msu_maf.vcf.gz
+VCF=/mnt/research/josephslab/Maya/capsella/vcf/filtered/CBP_allsites_msu_maf_SNPsOnly.vcf.gz
 POPS=/mnt/home/wils1582/capsella_introgression/pixy_pops_NYC_ownpop.txt
-OUTDIR=/mnt/scratch/wils1582/4-fold_pi
+OUTDIR=/mnt/scratch/wils1582/4-fold_pi_1-based
 PREFIX=nyc_allSites_CBP
 CHROM=$(sed -n "$SLURM_ARRAY_TASK_ID"p /mnt/scratch/wils1582/degenotate_out/scaffold_names.txt)
 
 # Optional VARS
-SITES=/mnt/scratch/wils1582/degenotate_out/degeneracy-4-fold-sites.txt 
+#SITES=/mnt/scratch/wils1582/degenotate_out/degeneracy-4-fold-sites.txt 
+#
+# PIXY expects 1-indexed, not 0-indexed bed files so I created that file like so
+# awk -d\t '$2=$2+1, $3=$3+1' degeneracy-4-fold_test.bed > degeneracy-4-fold-sites-1-based.bed
+# where test.bed is just a copy of degeneracy-4-fold_sites.bed
+BED=/mnt/scratch/wils1582/degenotate_out/degeneracy-4-fold-sites-1-based.bed
 
 # generate output space
+mkdir -p $OUTDIR
+
+# # calculate pi at 4-fold degenerate sites
+#  pixy --stats pi \
+# 	--sites_file $SITES \
+# 	--window_size 1 \
+# 	--chromosomes "$CHROM" \
+#  --vcf $VCF \
+#  --populations $POPS \
+#  --n_cores 10 \
+#  --output_folder "$OUTDIR" \
+#  --output_prefix "$PREFIX"_"$CHROM" \
+# 	--bypass_invariant_check 'yes'
 
 # calculate pi at 4-fold degenerate sites
  pixy --stats pi \
-	--sites_file $SITES \
-	--window_size 1 \
+	--sites_file $BED \
 	--chromosomes "$CHROM" \
  --vcf $VCF \
  --populations $POPS \
