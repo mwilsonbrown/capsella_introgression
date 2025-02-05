6f7efb33b40fdf5b4dd861d97d5bf88a633d44d9
Maya Wilson Brown <wils1582@msu.edu>
2025-02-05 16:31:06 -0500

    update script for calc pi in intro reg; add multiinter output for introgressed CBP; add pi output

--- popstats_introgressed.sh
+++ popstats_introgressed.sh
@@ -11,7 +11,6 @@
 #SBATCH --mem-per-cpu=32G
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=wils1582@msu.edu
-#SBATCH --array=1-16%3
 #SBATCH --output=/mnt/scratch/wils1582/slurm/slurm-%A.out
 #
 # Population statistics on introgressed regions with PIXY on C. bursa-pastoris
@@ -30,7 +29,7 @@ POPS=/mnt/home/wils1582/capsella_introgression/pixy_pops_NYC_ownpop.txt
 OUTDIR=/mnt/scratch/wils1582/popstats_intro
 PREFIX=all_multiinter_popstats
 # Set BED file as first 3 columns from multiinter output file (ommitting the first line which is a header)
-BED=$(tail -n +2 /mnt/scratch/wils1582/multiinter_bed/all_multiinter_rubella.txt | cut -f1,2,3)
+BED=/mnt/scratch/wils1582/multiinter_bed/all_multiinter_rubella.bed
 
 # generate output space
 #mkdir -p $OUTDIR
