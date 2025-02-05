c47efe2bf542c731e70cabba145bc8eb8cdb5f68
mwilsonbrown <wils1582@msu.edu>
2025-02-05 10:39:36 -0500

    add investigation of Pi values in VCF at 4-fold sites; initiate calc of pi in introgressed regions

--- popstats_introgressed.sh
+++ popstats_introgressed.sh
@@ -1,10 +1,10 @@
 #!/bin/bash --login
 #
-#SBATCH --job-name=Pi4_bed_1based
+#SBATCH --job-name=Pi_introgressed
 #SBATCH --nodes=10
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=1-15:00:00
+#SBATCH --time=0-15:00:00
 #SBATCH --partition=josephsnodes
 #SBATCH --account=josephsnodes
 #SBATCH --export=NONE
@@ -12,38 +12,34 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=wils1582@msu.edu
 #SBATCH --array=1-16%3
-#SBATCH --output=/mnt/scratch/wils1582/slurm/slurm-%A_%a.out
+#SBATCH --output=/mnt/scratch/wils1582/slurm/slurm-%A.out
 #
-# Population statistics with PIXY on C. bursa-pastoris
-# January 28, 2025
+# Population statistics on introgressed regions with PIXY on C. bursa-pastoris
+# Feb 5, 2025
 
 # PIXY population file generation in capsella_introgression_popgen.Rmd
-cd /mnt/scratch/wils1582/4-fold_pi
+cd /mnt/scratch/wils1582/popstats_intro
 
 # initialize conda environment
 #conda activate pixy
 source /mnt/home/wils1582/miniconda3/bin/activate pixy
 
 #### VARS
-VCF=/mnt/research/josephslab/Maya/capsella/vcf/filtered/CBP_allSites_msu_maf.vcf.gz
+VCF=/mnt/research/josephslab/Maya/capsella/vcf/filtered/CBP_allSites_msu_maf_reheader.vcf.gz
 POPS=/mnt/home/wils1582/capsella_introgression/pixy_pops_NYC_ownpop.txt
-OUTDIR=/mnt/scratch/wils1582/4-fold_pi
-PREFIX=nyc_4-fold_CBP
-CHROM=$(sed -n "$SLURM_ARRAY_TASK_ID"p /mnt/scratch/wils1582/whole_genome_popgen/CBP_allSites_msu_maf_chromlist.txt)
-# Optional VARS
-SITES=/mnt/scratch/wils1582/degenotate_out/1-based_4-fold_sites_CBP_allSites_msu_maf.txt
+OUTDIR=/mnt/scratch/wils1582/popstats_intro
+PREFIX=all_multiinter_popstats
+# Set BED file as first 3 columns from multiinter output file (ommitting the first line which is a header)
+BED=$(tail -n +2 /mnt/scratch/wils1582/multiinter_bed/all_multiinter_rubella.txt | cut -f1,2,3)
 
 # generate output space
 #mkdir -p $OUTDIR
 
-# calculate pi at 4-fold degenerate sites
+# calculate pi within introgressed regions
  pixy --stats pi \
-	--window_size 1 \
-	--sites_file $SITES \
-	--chromosomes "$CHROM" \
-	 --vcf $VCF \
+	--bed_file $BED \
+	--vcf $VCF \
  	--populations $POPS \
  	--n_cores 10 \
  	--output_folder "$OUTDIR" \
- 	--output_prefix "$PREFIX"_"$CHROM" \
-	--bypass_invariant_check 'yes'
+ 	--output_prefix "$PREFIX" \
